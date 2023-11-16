09ffadb3d2a09585e11ebe018ca0d7312895f80d
fvgbhnjm <114211676+fvgbhnjm@users.noreply.github.com>
2023-11-15 23:12:34 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkPassword() {
+    var enteredPassword = document.getElementById('password').value;
+    var correctPassword = 'yourPassword'; // Replace with your actual password
+
+    if (enteredPassword === correctPassword) {
+        document.getElementById('password-prompt').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    } else {
+        alert('Incorrect password. Please try again.');
+    }
+}
